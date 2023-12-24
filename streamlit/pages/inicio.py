5d11ad17c2a641c031004da6a2f735a4363cd1c9
miguelvaldess <128128049+miguelvaldess@users.noreply.github.com>
2023-12-24 14:59:07 +0100

    Update inicio.py

--- streamlit/pages/inicio.py
+++ streamlit/pages/inicio.py
@@ -12,7 +12,7 @@ def mostrar_inicio():
     st.markdown("""
     El proyecto se centra en analizar y visualizar datos de casos de COVID-19 reportados en diferentes Comunidades Autónomas de España. Utilizaremos un conjunto de datos proporcionado por los servicios de salud, que incluye información detallada sobre la cantidad de casos, métodos de diagnóstico utilizados y fechas de reporte. El objetivo es crear un dashboard interactivo que permita explorar y comprender la evolución de la enfermedad en diferentes regiones, identificar tendencias temporales, comparar el número de casos entre Comunidades Autónomas y analizar la distribución de métodos de diagnóstico. Nuestro objetivo se basa en analizar los casos registrados y compararlos con las pruebas que se realizaban para verificar la vericidad de estos y el peso de cada tipo de prueba.
     """)
-    st.image("foto.png", use_column_width=True, width=200)
+    st.image("pages/foto.png", use_column_width=True, width=200)
     st.markdown("[Referencia de la imagen](https://www.aa.com.tr/es/mundo/españa-registra-más-de-1000-nuevos-contagios-por-covid-19-en-un-solo-día/1768238)")
 
 

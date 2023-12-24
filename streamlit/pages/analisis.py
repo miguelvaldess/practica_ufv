e97f59c2d0feec9c6b83d8da8260285958c832a0
miguelvaldess <128128049+miguelvaldess@users.noreply.github.com>
2023-12-24 14:59:26 +0100

    Update analisis.py

--- streamlit/pages/analisis.py
+++ streamlit/pages/analisis.py
@@ -11,7 +11,7 @@ def mostrar_analisis():
     - Identificación de tendencias temporales en la propagación de casos.
     - Comparaciones entre Comunidades Autónomas en cuanto a la detección de casos por diferentes métodos de prueba.
     - Correlaciones entre los métodos de prueba y la cantidad de casos reportados.""")
-    st.image("foto1.png")
+    st.image("pages/foto1.png")
     st.markdown("[Referencia de la imagen](https://www.rtve.es/noticias/coronavirus-covid-19/)")
     st.markdown("""
     El análisis de estos datos puede proporcionar una comprensión profunda de la evolución y distribución de la enfermedad en las diferentes regiones de España. Algunos de los enfoques de análisis y posibles usos incluyen: Identificación de Tendencias Temporales, Comparación entre Comunidades Autónomas, Análisis de Métodos de Detección, Correlaciones y Factores de Influencia, Predicciones y Modelado, y Seguimiento de Estrategias de Salud Pública. Estos análisis proporcionarán información valiosa para los epidemiólogos, autoridades sanitarias y el público en general, facilitando la toma de decisiones informadas y el desarrollo de estrategias efectivas para combatir la propagación de la enfermedad.
